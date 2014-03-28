97986afb3180986b91ef3ec0462601038fa66956
Foggalong <joshua.h.fogg@gmail.com>
2014-03-28 21:43:11 +0000

    Getting there130 165 M

--- hc-fix.py
+++ hc-fix.py
@@ -4,7 +4,6 @@
 # This updater code is released under the GPL v3. The license
 # is included with the rest of the icon suit on GitHub. This
 # script is written and maintained by Joshua Fogg for Numix.
-
 from os import environ, execlpe, geteuid, listdir
 from os.path import expanduser
 from sys import executable, argv
@@ -18,17 +17,47 @@ if euid == 0:
 else:
 	pass
 
-# no rooting: /home/josh/.local/share/applications/
-print("Fixing local application icons...")
-local_launchers = listdir(expanduser("~")+"/.local/share/applications")
-
-# List of known local applications that use hardcoded icons
-local_hardcoded = [
+# List of known apps that use hardcoded icons
+hardcoded = [
+	# .desktop name needed ["Android Studio", " /opt/android-studio/bin/idea.png", " androidstudio"]
+	# .desktop name needed ["Aptik", " /usr/share/pixmaps/aptik.png", "aptik"]
+	# .desktop name needed ["Arista", " /usr/share/arista/ui/icon.svg", "arista"]
 	["Bastion.desktop","steam","steam_icon_107100"],
+	# .desktop name needed ["Conky Manager", " /usr/share/pixmaps/conky-manager.png", " conky-manager"]
+	# .desktop name needed ["Dogecoin-qt (AUR)", "/usr/share/pixmaps/dogecoin.png", "dogecoin"]
 	["Dota 2.desktop","steam","steam_icon_570"],
+	# .desktop name needed ["Driver Manager", "/usr/share/icons/hicolor/scalable/apps/driver-manager.svg", "jockey"]
+	# .desktop name needed ["Easy Life", "/usr/share/pixmaps/easylife.png", "easylife"]
+	# .desktop name needed ["Fade In", "/usr/share/fadein/icon_app/fadein_icon_128x128.png", "fadein"]
+	# .desktop name needed ["Format Junkie", "/opt/extras.ubuntu.com/formatjunkie/pixmap/fjt.png", "fjt"]
+	# .desktop name needed ["Gcolor2", " /usr/share/pixmaps/gcolor2/gcolor2.xpm", "gcolor2"]
+	# .desktop name needed ["Gespeaker", "/usr/share/gespeaker/data/icons/gespeaker.svg", "gespeaker"]
+	# .desktop name needed ["GNOME Weather", "org.gnome.Weather.Application", "gnome-weather"]
+	# .desktop name needed ["GNUcview", "/usr/share/pixmaps/guvcview/guvcview.png", "guvcview"]
+	# .desktop name needed ["GNU Octave", "/usr/share/octave/3.6.4/imagelib/octave-logo.svg", "octave"]
+	# .desktop name needed ["Graphic Network Simulator", "/usr/share/pixmaps/gns3.xpm", "gns"]
+	# .desktop name needed ["Guake", "/usr/share/pixmaps/guake/guake.png", "guake"]
+	# .desktop name needed ["HipChat", "hipchat.png", "hipchat"]
+	# .desktop name needed ["Intel Graphics Installer", "/usr/share/intel-linux-graphics-installer/images/logo.png", "intel-installer"]
+	# .desktop name needed ["IntelliJ IDEA", "/opt/idea-IC/bin/idea.png", "idea"]
 	["Kerbal Space Program.desktop","steam","steam_icon_220200"],
 	["Left 4 Dead 2.desktop","steam","steam_icon_550"],
 	["Left 4 Dead 2 Beta.desktop","steam","steam_icon_223530"],
+	# .desktop name needed ["Lightworks", "/usr/share/lightworks/Icons/App.png", "lightworks"]
+	# .desktop name needed ["Lucky Backup", "/usr/share/pixmaps/luckybackup.png", "luckybackup"]
+	# .desktop name needed ["Master PDF Editor", "/opt/master-pdf-editor/master-pdf-editor.png", "master-pdf-editor"]
+	# .desktop name needed ["My Weather Indicator", "/opt/extras.ubuntu.com/my-weather-indicator/share/pixmaps/my-weather-indicator.png", "indicator-weather"]
+	# .desktop name needed ["Netbeans", "/usr/share/netbeans/7.0.1/nb/netbeans.png", "netbeans"]
+	# .desktop name needed ["Ninja IDE", "/usr/share/ninja-ide/img/icon.png", "ninja-ide"]
+	# .desktop name needed ["Nitro", "/usr/share/nitrotasks/media/nitrotasks.png", "nitrotasks"]
+	# .desktop name needed ["OmegaT", "/usr/share/omegat/images/OmegaT.xpm", "omegat"]
+	# .desktop name needed ["Oracle SQL Developer", "/opt/oracle-sqldeveloper/icon.png", "N/A"]
+	# .desktop name needed ["PacmanXG", "/usr/share/pixmaps/pacmanxg.png", "pacmanxg"]
+	# .desktop name needed ["Pamac (Install)", "/usr/share/pamac/icons/32x32/apps/pamac.png", "system-software-install"]
+	# .desktop name needed ["Pamac (Update)", "/usr/share/pamac/icons/32x32/apps/pamac.png", "system-software-update"]
+	# .desktop name needed ["Pencil", "/usr/share/pencil/skin/classic/icon.svg", "pencil"]
+	# .desktop name needed ["PHP Storm", "PhpStorm-133.803/bin/webide.png", "phpstorm"]
+	# .desktop name needed ["Pycharm", "/home/radio/Descargas/pycharm-community-3.1.1/bin/pycharm.png", "pycharm"]
 	["python2.6.desktop","/usr/share/pixmaps/python2.6.xpm","python2.6"],
 	["python2.7.desktop","/usr/share/pixmaps/python2.7.xpm","python2.7"],
 	["python3.0.desktop","/usr/share/pixmaps/python3.0.xpm","python3.0"],
@@ -36,9 +65,22 @@ local_hardcoded = [
 	["python3.2.desktop","/usr/share/pixmaps/python3.2.xpm","python3.2"],
 	["python3.3.desktop","/usr/share/pixmaps/python3.3.xpm","python3.3"],
 	["python3.4.desktop","/usr/share/pixmaps/python3.4.xpm","python3.4"],
+	# .desktop name needed ["Robomongo", "robomongo.png", "robomongo"]
+	# .desktop name needed ["SmartGitHG", "smartgithg.png", "smartgithg"]
+	# .desktop name needed ["Springseed", "/usr/share/pixmaps/springseed/springseed.svg", "springseed"]
+	# .desktop name needed ["Synergy", "/usr/share/icons/synergy.ico", "synergy"]
+	# .desktop name needed ["TimeShift", "/usr/share/pixmaps/timeshift.png", "timeshift"]
+	# .desktop name needed ["Tomate", "/usr/share/tomate/media/tomate.png", "tomate"]
+	# .desktop name needed ["Valentina Studio", "/opt/VStudio/Resources/vstudio.png", "vstudio"]
+	# .desktop name needed ["YouTube-DL GUI", "/usr/share/pixmaps/youtube-dlg.png", "youtube-dl"]
 ]
 
-for launcher in local_hardcoded:
+# Set up for local fixes
+print("Fixing local application icons...")
+local_launchers = listdir(expanduser("~")+"/.local/share/applications")
+
+# Fixes locally located launchers 
+for launcher in hardcoded:
 	if launcher[0] in local_launchers:
 		print("Fixing "+launcher[0].replace(".desktop","..."))
 		desktop_file = open(expanduser("~")+"/.local/share/applications/"+launcher[0], 'r+')
@@ -63,6 +105,7 @@ Because most launchers are in /usr/share/applications/
 fixing their hardcoded icon lines requites root privlages.
 You can exit the script safely at this point if you wish.\n"""
 
+# Aquires root
 euid = geteuid()
 if euid != 0:
     print(warning_message)
@@ -71,15 +114,12 @@ if euid != 0:
     execlpe('sudo', *args)
 
 print("\nAquired root!")
+# Set up for global fixes
 print("Fixing global application icons...")
 global_launchers = listdir("/usr/share/applications")
 
-# List of known local applications that use hardcoded icons
-global_hardcoded = [
-	["app.desktop","old","numix"],
-]
-
-for launcher in global_hardcoded:
+# Fixes globally located launchers
+for launcher in hardcoded:
 	if launcher[0] in global_launchers:
 		print("Fixing "+launcher[0].replace(".desktop","..."))
 		desktop_file = open("/usr/share/applications"+launcher[0], 'r+')
