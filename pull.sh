f00007efba381272c13a47f96f341145da826320
Siddharth <sidgroks@gmail.com>
2015-07-05 02:45:27 +0530

    Added Sample Spec

--- pull.sh
+++ pull.sh
@@ -10,8 +10,8 @@
 url=$1
 version=$2
 cd SOURCES
-wget ${url} -O ${version}.zip
+wget $url -O ${version}.zip
 folder=$(unzip ${version}.zip | grep -m1 'creating:' | cut -d' ' -f5-)
-mv $folder Numix-themes
+ 
 
 
