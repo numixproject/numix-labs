eaf807d3301f0b51dda034cdfb16427ec2483a5e
Siddharth <sidgroks@gmail.com>
2015-07-10 22:42:21 +0530

    Working Version Check and Package Pull

--- main.py
+++
@@ -1,7 +0,0 @@
-import sidLibs
-
-
-print "Enter URL"
-url=raw_input("> ")
-ret = sidLibs.pullVersion(url)
-sidLibs.dataEx(ret[0],ret[1])
--- packg.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-cp .specs/example.spec SPECS/numix.spec
